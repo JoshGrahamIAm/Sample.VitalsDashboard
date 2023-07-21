0bb4c26eede2e4d035d6077cfa4610be96eb9a2e
Josh Graham <joshgrahamtn@gmail.com>
2023-07-21 15:28:41 -0400

    cleanup formatting

--- Program.cs
+++ Program.cs
@@ -6,26 +6,21 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//initialize datacontext
+//Manage user secrets for connection string.   
+var connectionString = builder.Configuration.GetConnectionString("AppDb");
+builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseSqlServer(connectionString));
 
 builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddSignalR();
-
-//add datacontext to ioc
-//Manage user secrets for connection string.   
-var connectionString = builder.Configuration.GetConnectionString("AppDb");
-builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseSqlServer(connectionString)) ;
 
 //add password hashing service to DI container
 builder.Services.AddScoped<ICustomPasswordHasher, CustomPasswordHasher>();
-
-
-builder.Services.AddControllersWithViews();
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie();
 
-//create heartrate data generator service
+//create random heartrate and o2 data generator service
 builder.Services.AddHostedService<HeartRateGenerator>();
 
 var app = builder.Build();
@@ -33,17 +28,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
-
 
 app.UseAuthentication(); // Add authentication middleware
 app.UseAuthorization();
